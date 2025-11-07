7f31a2d89fbcd29448a8aad8ec8733aa60377e67
sia007 <sia000777@gmail.com>
2025-11-07 16:14:11 -0500

    work done

--- cinema-seats/vite.config.js
+++ cinema-seats/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Replace 'cinema' with your repo name exactly
 export default defineConfig({
   plugins: [react()],
-  // base: '/your-repo-name/',
+  base: '/cinema/',   // <-- IMPORTANT
 })
